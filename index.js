6bfe026ebe6b649e3e4ce3abf098801c9548f34f
ikurotime <davidhuertasortiz@gmail.com>
2024-07-18 18:25:13 +0200

    refactor: Update index.js to include a smiley face in the response message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 import { serve } from '@hono/node-server'
 
 const app = new Hono()
-app.get('/', (c) => c.text('Hello Node.js!!!'))
+app.get('/', (c) => c.text('Hello Node.js!!! :)'))
 
 console.log("Server started")
 serve(app)
