e6110a2e6c92be8cbe245dda2f6b3906725c8445
ikurotime <davidhuertasortiz@gmail.com>
2024-07-18 16:35:24 +0200

    Github vps workflow example

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:18-alpine
+
+WORKDIR /app
+
+COPY package.json .
+COPY index.js .
+
+RUN npm install 
+
+CMD ["node", "index.js"]
---
+++ index.js
@@ -0,0 +1,8 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+app.get('/', (c) => c.text('Hello Node.js!'))
+
+console.log("Server started")
+serve(app)
